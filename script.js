0c5c266e94b25f49c2398fc1c79b4d0b17d50d10
nclbgrn <bugarinnicole9@gmail.com>
2025-03-25 21:22:26 -0600

    Add API

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const jokeElement = document.getElementById("joke");
     const titleElement = document.getElementById("title");
     const buttons = document.querySelectorAll("button");
-
+    
     async function fetchJoke(category = "Any") {
-       
+        const response = await fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`);
+        const data = await response.json();
+        
+        if (data.type === "single") {
+            jokeElement.textContent = data.joke;
+        } else {
+            jokeElement.textContent = `${data.setup} ... ${data.delivery}`;
+        }
+        
+        titleElement.textContent = category === "Pun" ? "A Random Pun" : `A Random ${category} Joke`;
+    }
+    
+    fetchJoke();
