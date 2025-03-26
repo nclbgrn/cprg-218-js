9ec0a730a25abe749cec6f8be4d0eb08bfa6088c
nclbgrn <bugarinnicole9@gmail.com>
2025-03-25 21:10:55 -0600

    Add css styling

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const jokeContainer = document.getElementById("joke");
-    const title = document.getElementById("title");
-    const buttons = document.querySelectorAll("button");
